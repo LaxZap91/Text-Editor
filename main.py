a8fbf7d841ea8db74cb533019c2c500a5daf349c
Zane Peterson <zanepeterson@gmail.com>
2024-08-20 00:01:28 -0700

    added save as command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import ttkbootstrap as ttk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 from text import TextBox
 
 
@@ -10,25 +10,27 @@ class TextEditorApp(ttk.Window):
         super().__init__("Text Editor", size=(500, 500))
 
         self.create_menu_bar()
+        self.create_text()
 
     def create_menu_bar(self):
         menu = ttk.Menu(self, tearoff=False)
         self.configure(menu=menu)
 
         file_menu = ttk.Menu(menu, tearoff=False)
+        file_menu.add_command(label="Open", command=self.open_command)
         file_menu.add_command(label="Save", command=self.save_command)
-        file_menu.add_command(label="Load", command=self.load_command)
+        file_menu.add_command(label="Save As", command=self.save_as_command)
         menu.add_cascade(label="File", menu=file_menu)
 
     def create_text(self):
         self.text = TextBox(self)
         self.text.pack(fill="both")
 
-    def load_command(self):
-        new_file_path = askopenfilename(filetypes=[("Text files", '.txt')])
-        if new_file_path.strip() == "":
+    def open_command(self):
+        file_path = askopenfilename(defaultextension='.txt', filetypes=[("Text documents (*.txt)", '*.txt')])
+        if file_path.strip() == "":
             return
-        self.file_path = new_file_path
+        self.file_path = file_path
 
         with open(self.file_path, mode='r') as file:
             self.text.delete(1.0, "end")
@@ -38,6 +40,15 @@ class TextEditorApp(ttk.Window):
         if self.file_path is not None:    
             with open(self.file_path, 'w') as file:
                 file.write(self.text.get(1.0, 'end'))
+        else:
+            self.save_as_command()
+    
+    def save_as_command(self):
+        file_path = asksaveasfilename(confirmoverwrite=True, defaultextension='.txt', filetypes=[("Text documents (*.txt)", '*.txt')])
+        if file_path.strip() == "":
+            return
+        with open(file_path, mode='w') as file:
+            file.write(self.text.get(1.0, 'end'))
 
 
 if __name__ == "__main__":
